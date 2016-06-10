908d05c9d79cb01b7d9562e5586e1d30b8e5776d
Adam Mulligan <adammulligan@gmail.com>
2016-06-10 13:53:04 +0100

    Load queues files on boot

--- app/src/loader.js
+++ app/src/loader.js
@@ -48,12 +48,34 @@ module.exports = (function () {
     }
   };
 
+  var loadQueue = function(app, path) {
+    logger.debug('Loading queues...');
+    var routesFiles = fs.readdirSync(path);
+    var existIndexRouter = false;
+    routesFiles.forEach(function(file) {
+      if (/^\..*/.test(file)) { return; }
+
+      var newPath = path ? (path + '/' + file) : file;
+      var stat = fs.statSync(newPath);
+
+      if (!stat.isDirectory()) {
+        if (file.lastIndexOf('Queue.js') !== -1) {
+          logger.debug('Loading queue %s', newPath);
+          require(newPath);
+        }
+      } else {
+        // is folder
+        loadQueue(app, newPath);
+      }
+    });
+  };
+
   var loadRoutes = function (app) {
     logger.debug('Loading routes...');
     loadAPI(app, routersPath);
+    loadQueue(app, routersPath);
     logger.debug('Loaded routes correctly!');
   };
-
 
   return {
     loadRoutes: loadRoutes
