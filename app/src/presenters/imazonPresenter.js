01e8d8a984752b5a3e840c9f3a437f795bde9d22
rrequero <rareq1987@gmail.com>
2016-08-18 16:26:43 +0200

    fix error

--- app/src/presenters/imazonPresenter.js
+++ app/src/presenters/imazonPresenter.js
@@ -3,10 +3,9 @@
 class ImazonPresenter {
 
     static * transform(results, layer, subscription) {
-        let result = {
-            alert_count_degradation: 0,
-            alert_count_deforestation: 0
-        };
+
+        results.alert_count_degradation = 0;
+        results.alert_count_deforestation = 0;
         if (results.value) {
             for (let i = 0, length = results.value.length; i < length; i++) {
                 if (results.value[i].dataType === 'degrad') {
