4289cd13267eba3a70eca91611d697581f248556
rrequero <rareq1987@gmail.com>
2016-08-23 19:05:27 +0200

    add migration fix

--- app/src/migrate/migrate.js
+++ app/src/migrate/migrate.js
@@ -24,7 +24,7 @@ var obtainData = function*(cursor) {
         method: 'GET',
         json: true
     });
-    if(response.statusCode !== 200){
+    if (response.statusCode !== 200) {
         logger.error(response);
         logger.info('Waiting 5 seconds and trying');
         yield sleep(5000);
@@ -39,42 +39,44 @@ var obtainData = function*(cursor) {
 
 let cacheUsers = {};
 var obtainNewUserId = function*(userId) {
-    try{
+    try {
         logger.info('Obtaining user with data', userId);
-        if(cacheUsers[userId]){
+        if (cacheUsers[userId]) {
             return cacheUsers[userId];
         }
         let result = yield microserviceClient.requestToMicroservice({
-          uri: '/user/oldId/' + userId,
-          method: 'GET',
-          json: true
+            uri: '/user/oldId/' + userId,
+            method: 'GET',
+            json: true
         });
 
 
         cacheUsers[userId] = result.body.data.id;
         return result.body.data.id;
-    }catch(e){
+    } catch (e) {
         logger.error(e);
     }
 };
 
-var createGeoJSON = function*(geojson) {
-    try{
+var createGeoJSON = function*(geojsonParam) {
+    try {
         logger.info('Creating geostore');
-
+        let json = {
+            geojson: geojsonParam
+        };
+        if (typeof geojsonParam === "string") {
+            json.geojson = JSON.parse(geojsonParam);
+        }
         let result = yield microserviceClient.requestToMicroservice({
-          uri: '/geostore/' + userId,
-          method: 'POST',
-          body: {
-              geojson: geojson
-          }
-          json: true
+            uri: '/geostore',
+            method: 'POST',
+            body: json,
+            json: true
         });
 
 
-
-        return result.body.data.id;
-    }catch(e){
+        return result;
+    } catch (e) {
         logger.error(e);
     }
 };
@@ -93,61 +95,69 @@ var oldDatasets = {
     'alerts/sad': 'imazon-alerts'
 };
 
-var tranformDataset = function(oldDataset){
-    if(oldDatasets[oldDataset]){
+var tranformDataset = function(oldDataset) {
+    if (oldDatasets[oldDataset]) {
         return oldDatasets[oldDataset];
     }
     return oldDataset;
 }
 
-var transformAndSaveData = function*(data){
+var transformAndSaveData = function*(data) {
     logger.info('Saving data');
-    if(data){
-         for(let i = 0, length = data.length; i < length; i++){
-             if(data[i].confirmed){
+    if (data) {
+        for (let i = 0, length = data.length; i < length; i++) {
+            if (data[i].confirmed) {
                 //  logger.debug('Saving data', data[i]);
-                 let userId = null;
-                 if(data[i].user_id){
-                     userId = yield obtainNewUserId(data[i].user_id);
-                 }else {
-                     logger.info('Subscription without user_id');
-                 }
-                 let language = data[i].language  || 'en';
+                let userId = null;
+                if (data[i].user_id) {
+                    userId = yield obtainNewUserId(data[i].user_id);
+                } else {
+                    logger.info('Subscription without user_id');
+                }
+                let language = data[i].language || 'en';
 
-                 if(!data[i].params.iso && !data[i].params.id1 && !data[i].params.wdpaid && !data[i].params.use && !data[i].params.useid && !data[i].params.ifl && data[i].params.fl_id1 && !data[i].params.geostore){
-                     let geostore = yield createGeoJSON(data[i].params.geom);
-                     data[i].geostore = geostore.id;
-                 }
-
-                 yield new Subscription({
-                     name: data[i].name,
-                     confirmed: data[i].confirmed,
-                     resource:{
-                         type: 'EMAIL',
-                         content: data[i].email
-                     },
-                     userId: userId,
-                     createdAt: data[i].created,
-                     datasets: [tranformDataset(data[i].topic)],
-                     language: language.toLowerCase(),
-                     params:{
-                         iso:{
-                             country: data[i].iso || data[i].params.iso,
-                             region: data[i].id1 || data[i].params.id1
-                         },
-                         geostore: data[i].geostore || data[i].params.geostore,
-                         wdpaid: data[i].wdpaid || data[i].params.wdpaid,
-                         use: data[i].use || data[i].params.use,
-                         useId: data[i].useid || data[i].params.useid,
-                         ifl: data[i].ifl || data[i].params.ifl,
-                         fl_id1: data[i].fl_id1 || data[i].params.fl_id1
+                if (!data[i].params.iso && !data[i].params.id1 && !data[i].params.wdpaid && !data[i].params.use && !data[i].params.useid && !data[i].params.ifl && !data[i].params.fl_id1 && !data[i].params.geostore) {
+                    if(!data[i].params.geom){
+                        continue;
                      }
-                 }).save();
+                     let geostore = yield createGeoJSON(data[i].params.geom);
+                     if(!geostore.body.data){
+                        logger.error('Is not correct');
+                        logger.error(data[i].params.geom);
+                        continue;
+                     }
+                     data[i].geostore = geostore.body.data.id;
+                }
 
-             } else {
-                 logger.debug('not confirmed');
-             }
-         }
+                yield new Subscription({
+                    name: data[i].name,
+                    confirmed: data[i].confirmed,
+                    resource: {
+                        type: 'EMAIL',
+                        content: data[i].email
+                    },
+                    userId: userId,
+                    createdAt: data[i].created,
+                    datasets: [tranformDataset(data[i].topic)],
+                    language: language.toLowerCase(),
+                    params: {
+                        iso: {
+                            country: data[i].iso || data[i].params.iso,
+                            region: data[i].id1 || data[i].params.id1
+                        },
+                        geostore: data[i].geostore || data[i].params.geostore,
+                        wdpaid: data[i].wdpaid || data[i].params.wdpaid,
+                        use: data[i].use || data[i].params.use,
+                        useId: data[i].useid || data[i].params.useid,
+                        ifl: data[i].ifl || data[i].params.ifl,
+                        fl_id1: data[i].fl_id1 || data[i].params.fl_id1
+                    }
+                }).save();
+
+            } else {
+                logger.debug('not confirmed');
+            }
+        }
     } else {
         logger.ingo('Empty list');
     }
@@ -163,9 +173,9 @@ var migrate = function*() {
         let element = null;
         let model, idConn = null;
         yield transformAndSaveData(data.subscriptions);
-        if(data.cursor){
+        if (data.cursor) {
             data = yield obtainData(data.cursor);
-        } else {
+        } else  {
             data = null;
         }
     }
