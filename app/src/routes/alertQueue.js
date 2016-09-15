b83bb0e404537accdf3d7e1d2e1c6bd9c70a204e
rrequero <rareq1987@gmail.com>
2016-09-15 10:15:53 +0200

    add timeout

--- app/src/routes/alertQueue.js
+++ app/src/routes/alertQueue.js
@@ -14,7 +14,7 @@ class AlertQueue {
   constructor() {
     logger.debug('Initializing queue with provider %s ', `redis://${config.get('redisLocal.host')}:${config.get('redisLocal.port')}`);
     this.asynClient = new AsyncClient(AsyncClient.REDIS, {
-        url: `redis://${config.get('redisLocal.host')}:${config.get('redisLocal.port')}`
+      url: `redis://${config.get('redisLocal.host')}:${config.get('redisLocal.port')}`
     });
 
 
@@ -23,34 +23,45 @@ class AlertQueue {
     channel.subscribe();
   }
 
-  * processMessage(channel, message) {
-    logger.info('Processing alert');
-    let layerSlug = MessageProcessor.getLayerSlug(message),
-        begin = MessageProcessor.getBeginDate(message),
-        end   = MessageProcessor.getEndDate(message);
-
-    logger.debug('Params in message', layerSlug, begin, end);
-    let subscriptions = yield SubscriptionService.getSubscriptionsByLayer(
-      layerSlug);
-    logger.debug('Subscriptions obtained', subscriptions);
-    try {
-      logger.info('Sending alerts for', layerSlug, begin.toISOString(), end.toISOString());
+  static sendPack(subscriptions, i) {
 
       let channel = this.asynClient.toChannel(ALERT_POST_CHANNEL);
-      subscriptions.forEach(function(subscription) {
-        let config = {
-          layer_slug: layerSlug,
-          subscription_id: subscription._id,
-          begin: begin,
-          end: end
-        };
+      setTimeout(function(){
+          logger.info(`Sending pack of ${i*10} to ${(i*10)+10}`);
+          for(let j = i * 10; j < ((i*10) +10); j++) {
+              let config = {
+                layer_slug: layerSlug,
+                subscription_id: subscription._id,
+                begin: begin,
+                end: end
+              };
 
-        channel.emit(JSON.stringify(config));
-      });
-    } catch (e) {
-      logger.error('Error sending subscription mail', e);
-    }
+              channel.emit(JSON.stringify(config));
+          }
+          AlertQueue.sendPack(subscriptions, i++);
+      }, 60000);
   }
-}
 
-module.exports = new AlertQueue();
+  *
+  processMessage(channel, message) {
+      logger.info('Processing alert');
+      let layerSlug = MessageProcessor.getLayerSlug(message),
+        begin = MessageProcessor.getBeginDate(message),
+        end = MessageProcessor.getEndDate(message);
+
+      logger.debug('Params in message', layerSlug, begin, end);
+      let subscriptions = yield SubscriptionService.getSubscriptionsByLayer(
+        layerSlug);
+      logger.debug('Subscriptions obtained', subscriptions);
+      try {
+        logger.info('Sending alerts for', layerSlug, begin.toISOString(), end.toISOString());
+
+
+        AlertQueue.sendPack(subscriptions);
+          catch (e) {
+            logger.error('Error sending subscription mail', e);
+          }
+        }
+      }
+
+      module.exports = new AlertQueue();
