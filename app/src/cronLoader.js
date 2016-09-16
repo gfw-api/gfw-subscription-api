d19f321597f972060d8a9f7b0d403dd504f5bad3
rrequero <rareq1987@gmail.com>
2016-09-16 11:26:01 +0200

    stop crons

--- app/src/cronLoader.js
+++ app/src/cronLoader.js
@@ -20,45 +20,46 @@ asynClient = asynClient.toChannel(CHANNEL);
 
 
 var load = function() {
-  taskConfig.forEach(function(task) {
-
-    /* THIS CODE IS TO TEST */
-    logger.info('Creating cron task for ' + task.name);
-
-
-    new CronJob(task.crontab, function() {
-        co(function *() {
-            logger.info('Publishing ' + task.dataset);
-            if (task.dataset !== 'viirs-active-fires'){
-                logger.info(`Checking if dataset '${task.dataset}' was updated`);
-                let result = yield UpdateService.checkUpdated(task.dataset);
-                if(result.updated) {
-                    asynClient.emit(JSON.stringify({
-                        layer_slug: task.dataset,
-                        begin_date: new Date(result.beginDate),
-                        end_date: new Date(result.endDate)
-                    }));
-                } else {
-                    logger.info(`${task.dataset} was not updated`);
-                }
-            } else {
-                let beginData = moment().subtract(task.gap.value, task.gap.measure).subtract(task.periodicity.value, task.periodicity.measure).toDate();
-                let endDate = moment().subtract(task.gap.value, task.gap.measure).toDate();
-
-                asynClient.emit(JSON.stringify({
-                    layer_slug: task.dataset,
-                    begin_date: beginData,
-                    end_date: endDate
-                }));
-            }
-        }).then(function(){}, function(err){
-            logger.error(err);
-        });
-
-
-
-    }, null, true, 'Europe/London');
-  });
+  logger.info('Stopped crons');
+  // taskConfig.forEach(function(task) {
+  //
+  //   /* THIS CODE IS TO TEST */
+  //   logger.info('Creating cron task for ' + task.name);
+  //
+  //
+  //   new CronJob(task.crontab, function() {
+  //       co(function *() {
+  //           logger.info('Publishing ' + task.dataset);
+  //           if (task.dataset !== 'viirs-active-fires'){
+  //               logger.info(`Checking if dataset '${task.dataset}' was updated`);
+  //               let result = yield UpdateService.checkUpdated(task.dataset);
+  //               if(result.updated) {
+  //                   asynClient.emit(JSON.stringify({
+  //                       layer_slug: task.dataset,
+  //                       begin_date: new Date(result.beginDate),
+  //                       end_date: new Date(result.endDate)
+  //                   }));
+  //               } else {
+  //                   logger.info(`${task.dataset} was not updated`);
+  //               }
+  //           } else {
+  //               let beginData = moment().subtract(task.gap.value, task.gap.measure).subtract(task.periodicity.value, task.periodicity.measure).toDate();
+  //               let endDate = moment().subtract(task.gap.value, task.gap.measure).toDate();
+  //
+  //               asynClient.emit(JSON.stringify({
+  //                   layer_slug: task.dataset,
+  //                   begin_date: beginData,
+  //                   end_date: endDate
+  //               }));
+  //           }
+  //       }).then(function(){}, function(err){
+  //           logger.error(err);
+  //       });
+  //
+  //
+  //
+  //   }, null, true, 'Europe/London');
+  // });
 };
 
 module.exports = {load: load};
