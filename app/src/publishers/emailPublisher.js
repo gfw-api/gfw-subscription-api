18187122cabe79cd0b4e8968ba06a3ec21ed2a7f
Sergio Gordillo <sgordillogallardo@gmail.com>
2016-12-09 11:03:19 +0100

    fix unsubscribe redirect

--- app/src/publishers/emailPublisher.js
+++ app/src/publishers/emailPublisher.js
@@ -25,9 +25,9 @@ class EmailPublisher {
   }
 
   static sendStats(emails, stats) {
-    logger.info('Publishing email with stats', stats);
+    logger.warn('Publishing email with stats', stats);
     let template = 'subscriptions-stats';
-    logger.info('MAIL TEMPLATE', template);
+    logger.warn('MAIL TEMPLATE', template);
     let recipients = emails.map(el => ({address: {email: el}}));
     mailService.sendMail(template, stats, recipients);
   }
