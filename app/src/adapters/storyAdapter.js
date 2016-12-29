734a69e74a7458ded02602adf8bcdc6775c00873
rrequero <rareq1987@gmail.com>
2016-12-29 10:29:51 +0100

    add more info in stories

--- app/src/adapters/storyAdapter.js
+++ app/src/adapters/storyAdapter.js
@@ -4,6 +4,19 @@ var logger = require('logger');
 var config = require('config');
 var UrlService = require('services/urlService');
 
+const DOMAIN_IMAGE = 'http://gfw2stories.s3.amazonaws.com/uploads/';
+
+var searchImage = function(story){
+  if (story.media){
+    for (let i= 0, length = story.media.length; i < length; i++) {
+      if (story.media[i].mimeType && story.media[i].url){
+        return DOMAIN_IMAGE + story.media[i].url;
+      }
+    }
+  }
+  return null;
+}
+
 class StoryAdapter {
 
   constructor(results) {
@@ -15,8 +28,10 @@ class StoryAdapter {
     if (this.results && this.results.length > 0) {
       for (let i = 0, length = this.results.length; i < length; i++) {
         stories.push({
-          name: this.results[i].name,
-          url: UrlService.flagshipUrl('/stories/'+this.results[i].id)
+          title: this.results[i].title,
+          description: this.results[i].description.substring(0, 350),
+          url: UrlService.flagshipUrl('/stories/'+this.results[i].id),
+          image: searchImage(this.results[i])
         });
       }
     }
