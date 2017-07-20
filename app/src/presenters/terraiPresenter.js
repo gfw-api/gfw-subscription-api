d06b86d6ed153de35eb74abb36a08f6adab53f6d
pjosh <jorgegar@gmail.com>
2017-07-20 13:37:34 +0200

    Format alerts points properly

--- app/src/presenters/terraiPresenter.js
+++ app/src/presenters/terraiPresenter.js
@@ -1,5 +1,5 @@
 'use strict';
-var logger = require('logger');
+const logger = require('logger');
 const ctRegisterMicroservice = require('ct-register-microservice-node');
 
 class TerraiPresenter {
@@ -7,11 +7,9 @@ class TerraiPresenter {
   static * transform(results, layer, subscription) {
     results.alert_count = results.value;
     let uri = '/query' + results.downloadUrls.csv.split('download')[1];
-    const geostore =  /geostore=([a-z0-9]*)/g.exec(uri)[1];
-    uri = uri.split('&format')[0].replace('ORDER BY year, day', ' order by day desc, year desc limit 10&geostore=') + geostore;
+    uri = uri.split('&format')[0].replace('ORDER BY year, day', ' ORDER BY day DESC, year DESC LIMIT 10');
 
     logger.info('Last alerts endpoint ', uri);
-
     try {
       let alerts = yield ctRegisterMicroservice.requestToMicroservice({
           uri: uri,
@@ -19,12 +17,18 @@ class TerraiPresenter {
           json: true
       });
       results.alerts = alerts.data.map(el => {
-        el.date = new Date(new Date(el.year, 0, 1).getTime() + el.day*24*3600*1000);
-        return el;
+        const date = new Date(new Date(el.year, 0, 1).getTime() + el.day*24*3600*1000)
+            .toISOString()
+            .split('T');
+        return {
+          acq_date: date[0],
+          acq_time: date[1].substr(0, 8),
+          latitude: el.lat,
+          longitude: el.long
+        };
       });
     } catch (err) {
       logger.error(err);
-      //throw(err);
       results.alerts = [];
     }
     logger.info('Terrai P Results ', results);
