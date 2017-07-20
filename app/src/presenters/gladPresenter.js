0a113e8c377a211919a5a7ae757342c5fd94580f
pjosh <jorgegar@gmail.com>
2017-07-20 13:30:00 +0200

    Format alerts points properly

--- app/src/presenters/gladPresenter.js
+++ app/src/presenters/gladPresenter.js
@@ -1,5 +1,5 @@
 'use strict';
-var logger = require('logger');
+const logger = require('logger');
 const ctRegisterMicroservice = require('ct-register-microservice-node');
 
 class GLADPresenter {
@@ -7,10 +7,9 @@ class GLADPresenter {
   static * transform(results, layer, subscription) {
     results.alert_count = results.value;
     let uri = '/query' + results.downloadUrls.csv.split('download')[1];
-    uri = uri.split('&format')[0].replace('&geostore', ' order by julian_day desc, year desc limit 10&geostore');
+    uri = uri.split('&format')[0].replace('ORDER BY year, julian_day', ' ORDER BY julian_day DESC, year DESC LIMIT 10');
 
     logger.debug('Last alerts endpoint ', uri);
-
     try {
       let alerts = yield ctRegisterMicroservice.requestToMicroservice({
           uri: uri,
@@ -18,12 +17,18 @@ class GLADPresenter {
           json: true
       });
       results.alerts = alerts.data.map(el => {
-        el.date = new Date(new Date(el.year, 0, 1).getTime() + el.julian_day*24*3600*1000);
-        return el;
+        const date = new Date(new Date(el.year, 0, 1).getTime() + el.julian_day*24*3600*1000)
+            .toISOString()
+            .split('T');
+        return {
+            acq_date: date[0],
+            acq_time: date[1].substr(0, 8),
+            latitude: el.lat,
+            longitude: el.long
+        };
       });
     } catch (err) {
       logger.error(err);
-      //throw(err);
       results.alerts = [];
     }
     logger.info('Glad P Results ', results);
