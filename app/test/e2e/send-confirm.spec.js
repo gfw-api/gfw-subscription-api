42e47d3f4940e51057a1578e4fadc147cd935737
stanislavgalin <stasgalin@gmail.com>
2019-11-06 10:21:29 +0200

    added missing test cases

--- app/test/e2e/send-confirm.spec.js
+++ app/test/e2e/send-confirm.spec.js
@@ -1,20 +1,26 @@
 /* eslint-disable no-unused-vars,no-undef */
 const nock = require('nock');
 const Subscription = require('models/subscription');
+const config = require('config');
+const chai = require('chai');
+const redis = require('redis');
 const {
     createSubscription,
     ensureCorrectError,
     createAuthCases,
     getUUID,
+    sleep,
+    validRedisMessage
 } = require('./utils/helpers');
 const { createMockSendConfirmationSUB } = require('./utils/mock');
 const { ROLES } = require('./utils/test.constants');
 const { createRequest } = require('./utils/test-server');
-const chai = require('chai');
 
 const should = chai.should();
 
 const prefix = '/api/v1/subscriptions';
+
+const CHANNEL = config.get('apiGateway.queueName');
 
 nock.disableNetConnect();
 nock.enableNetConnect(process.env.HOST_IP);
@@ -28,6 +34,9 @@ describe('Send confirmation endpoint', () => {
         if (process.env.NODE_ENV !== 'test') {
             throw Error(`Running the test suite with NODE_ENV ${process.env.NODE_ENV} may result in permanent data loss. Please use NODE_ENV=test.`);
         }
+
+        this.channel = redis.createClient({ url: config.get('redis.url') });
+        this.channel.subscribe(CHANNEL);
 
         nock.cleanAll();
 
@@ -44,6 +53,8 @@ describe('Send confirmation endpoint', () => {
     it('Sending confirm subscription with provide loggedUser as not an object json string should fall', authCases.isLoggedUserJSONObject());
 
     it('Sending confirm subscription with being authenticated but with not existing subscription for user should fall', async () => {
+        this.channel.on('message', () => should.fail('should not be called'));
+
         createSubscription(ROLES.USER.id, getUUID());
         const response = await subscription
             .get('/41224d776a326fb40f000001/send_confirmation')
@@ -51,27 +62,43 @@ describe('Send confirmation endpoint', () => {
             .send();
         response.status.should.equal(404);
         ensureCorrectError(response.body, 'Subscription not found');
+
+        await sleep(1000);
     });
 
     it('Sending confirm subscription should return not found when subscription doesn\'t exist', async () => {
+        this.channel.on('message', () => should.fail('should not be called'));
+
         const response = await subscription
             .get('/41224d776a326fb40f000001/send_confirmation')
             .query({ loggedUser: JSON.stringify(ROLES.USER) })
             .send();
         response.status.should.equal(404);
         ensureCorrectError(response.body, 'Subscription not found');
+
+        await sleep(1000);
     });
 
     it('Sending confirm subscription should return bad request when id is not valid', async () => {
+        this.channel.on('message', () => should.fail('should not be called'));
+
         const response = await subscription
             .get('/123/send_confirmation')
             .query({ loggedUser: JSON.stringify(ROLES.USER) })
             .send();
         response.status.should.equal(400);
         ensureCorrectError(response.body, 'ID is not valid');
+
+        await sleep(1000);
     });
 
-    it('Sending confirmation subscription should redirect to flagship (happy case)', async () => {
+    it('Sending confirmation subscription should redirect to flagship and emit a redis message (happy case)', async () => {
+        this.channel.on('message', validRedisMessage({
+            template: 'subscription-confirmation-en',
+            application: 'gfw',
+            language: 'en',
+        }));
+
         createMockSendConfirmationSUB();
         const createdSubscription = await new Subscription(createSubscription(ROLES.USER.id)).save();
         const response = await subscription
@@ -80,10 +107,13 @@ describe('Send confirmation endpoint', () => {
             .send();
         response.status.should.equal(200);
         response.body.mockMessage.should.equal('Should redirect');
+
+        process.on('unhandledRejection', err => should.fail(err));
     });
 
-
     afterEach(() => {
+        process.removeAllListeners('unhandledRejection');
+        this.channel.removeAllListeners('message');
         Subscription.remove({}).exec();
 
         if (!nock.isDone()) {
