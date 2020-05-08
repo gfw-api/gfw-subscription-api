c2ce83450c456a4fa31074dcab289603d23b72d1
Tiago Garcia <tiagojsag@gmail.com>
2020-05-08 16:10:07 +0200

    Add mongoose connection debugging output

--- app/src/app.js
+++ app/src/app.js
@@ -134,6 +134,21 @@ async function init() {
         if (config.get('logger.level') === 'debug') {
             mongoose.set('debug', true);
         }
+
+        mongoose.connection.on('connecting', () => {
+            logger.debug('Mongoose attempting to connect');
+        });
+        mongoose.connection.on('connected', () => {
+            logger.debug('Mongoose connected to the initial server');
+        });
+        mongoose.connection.on('fullsetup', () => {
+            logger.debug('Mongoose connected to the primary server and at least a secondary server');
+        });
+        mongoose.connection.on('all', () => {
+            logger.debug('Mongoose connected to all servers');
+        });
+
+
         mongoose.connect(mongoUri, mongooseOptions, onDbReady);
     });
 }
