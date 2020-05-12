5b50501e737e4d2fda19bf160cef3a9717f57fe6
Tiago Garcia <tiagojsag@gmail.com>
2020-05-12 11:45:55 +0200

    Reset logging levels to info; fix typo

--- config/mongoose.js
+++ config/mongoose.js
@@ -7,7 +7,7 @@ const mongooseOptions = {
     useUnifiedTopology: false,
     readPreference: 'secondaryPreferred', // Has MongoDB prefer secondary servers for read operations.
     appname: 'subscriptions', // Displays the app name in MongoDB logs, for ease of debug
-    serverSelectionTimeoutMS: 10000, // Number of miliseconds the underlying MongoDB driver has to pick a server
+    serverSelectionTimeoutMS: 10000, // Number of milliseconds the underlying MongoDB driver has to pick a server
     loggerLevel: config.get('logger.level') // Logger level to pass to the MongoDB driver
 };
 
