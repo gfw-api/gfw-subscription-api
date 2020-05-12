d7bfbc612d9ea2d9e23d4002ceac921f3984aa63
Tiago Garcia <tiagojsag@gmail.com>
2020-05-12 10:08:01 +0200

    Use apps logger level as mongoose logger level

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,3 +1,5 @@
+const config = require('config');
+
 const mongooseOptions = {
     useNewUrlParser: true,
     useFindAndModify: false,
@@ -5,8 +7,8 @@ const mongooseOptions = {
     useUnifiedTopology: true,
     readPreference: 'secondaryPreferred', // Has MongoDB prefer secondary servers for read operations.
     appname: 'subscriptions', // Displays the app name in MongoDB logs, for ease of debug
-    serverSelectionTimeoutMS: 10000 // Number of miliseconds the underlying MongoDB driver has to pick a server
-
+    serverSelectionTimeoutMS: 10000, // Number of miliseconds the underlying MongoDB driver has to pick a server
+    loggerLevel: config.get('logger.level') // Logger level to pass to the MongoDB driver
 };
 
 module.exports = mongooseOptions;
