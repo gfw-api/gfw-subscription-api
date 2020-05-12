4096d05b14279e6ac3bb2bbb585fe49464d37032
Tiago Garcia <tiagojsag@gmail.com>
2020-05-12 12:30:49 +0200

    Reenable unified topology when connecting to mongodb

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const mongooseOptions = {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
-    useUnifiedTopology: false,
+    useUnifiedTopology: true,
     readPreference: 'secondaryPreferred', // Has MongoDB prefer secondary servers for read operations.
     appname: 'subscriptions', // Displays the app name in MongoDB logs, for ease of debug
     serverSelectionTimeoutMS: 10000, // Number of milliseconds the underlying MongoDB driver has to pick a server
