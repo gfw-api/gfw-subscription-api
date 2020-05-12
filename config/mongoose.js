27079c1eaa73654aef5778539e6bf682d630df01
Tiago Garcia <tiagojsag@gmail.com>
2020-05-12 08:57:50 +0200

    Attempt to have the latest mongoose play nice with reading from replicas

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,10 +3,10 @@ const mongooseOptions = {
     useFindAndModify: false,
     useCreateIndex: true,
     useUnifiedTopology: true,
-    readPreference: 'secondaryPreferred',
-    db: {
-        readPreference: 'secondaryPreferred'
-    }
+    readPreference: 'secondaryPreferred', // Has MongoDB prefer secondary servers for read operations.
+    appname: 'subscriptions', // Displays the app name in MongoDB logs, for ease of debug
+    serverSelectionTimeoutMS: 10000 // Number of miliseconds the underlying MongoDB driver has to pick a server
+
 };
 
 module.exports = mongooseOptions;
