2075a9bb23763def637f025c551e7cb1044312a8
Tiago Garcia <tiagojsag@gmail.com>
2020-05-12 11:26:26 +0200

    Disable mongo unified topology, see what happens...

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,7 @@ const mongooseOptions = {
     useNewUrlParser: true,
     useFindAndModify: false,
     useCreateIndex: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: false,
     readPreference: 'secondaryPreferred', // Has MongoDB prefer secondary servers for read operations.
     appname: 'subscriptions', // Displays the app name in MongoDB logs, for ease of debug
     serverSelectionTimeoutMS: 10000, // Number of miliseconds the underlying MongoDB driver has to pick a server
