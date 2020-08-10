d3936f30c9559e09798b31c78a3aaac8fc912a86
Henrique Pacheco <henrique.pacheco@vizzuality.com>
2020-08-10 16:41:22 +0100

    Failing tests for handling of subscription parameters for legacy subscriptions

--- app/test/e2e/email-notifications/glad-alerts-emails.spec.js
+++ app/test/e2e/email-notifications/glad-alerts-emails.spec.js
@@ -604,6 +604,54 @@ describe('GLAD alert emails', () => {
         }));
     });
 
+    it('Legacy subscription parameters are correctly handled, triggering a new email being queued using the correct email template with the correct data', async () => {
+        const subscriptionOne = await new Subscription(createSubscription(
+            ROLES.USER.id,
+            'glad-alerts',
+            {
+                params: {
+                    iso: {
+                        subRegion: null,
+                        region: null,
+                        country: null
+                    },
+                    wdpaid: null,
+                    use: null,
+                    useid: null,
+                    geostore: '423e5dfb0448e692f97b590c61f45f22'
+                }
+            },
+        )).save();
+
+        const { beginDate, endDate } = bootstrapEmailNotificationTests();
+        // Despite the payload of the params object, geostore dataset should be used
+        mockGLADAlertsQuery(3);
+
+        redisClient.on('message', (channel, message) => {
+            const jsonMessage = JSON.parse(message);
+            jsonMessage.should.have.property('template');
+            switch (jsonMessage.template) {
+
+                case 'forest-change-notification-glads-en':
+                    validateGLADNotificationParams(jsonMessage, beginDate, endDate, subscriptionOne);
+                    break;
+                case 'subscriptions-stats':
+                    assertSubscriptionStatsNotificationEvent(jsonMessage);
+                    break;
+                default:
+                    should.fail('Unsupported message type: ', jsonMessage.template);
+                    break;
+
+            }
+        });
+
+        await AlertQueue.processMessage(null, JSON.stringify({
+            layer_slug: 'glad-alerts',
+            begin_date: beginDate,
+            end_date: endDate
+        }));
+    });
+
     afterEach(async () => {
         redisClient.removeAllListeners();
         process.removeAllListeners('unhandledRejection');
