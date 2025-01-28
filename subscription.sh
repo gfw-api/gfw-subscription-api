2c2951c50ebc90786c06027611065a66bd0aac2b
Solomon Negusse <solomon.negusse@wri.org>
2025-01-28 23:08:31 +0300

    update docker compose command

--- subscription.sh
+++ subscription.sh
@@ -5,12 +5,12 @@ case "$1" in
         yarn start
         ;;
     develop)
-        type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
-        docker-compose -f docker-compose-develop.yml build && docker-compose -f docker-compose-develop.yml up
+        type docker compose >/dev/null 2>&1 || { echo >&2 "docker compose is required but it's not installed.  Aborting."; exit 1; }
+        docker compose -f docker-compose-develop.yml build && docker compose -f docker-compose-develop.yml up --abort-on-container-exit
         ;;
     test)
-        type docker-compose >/dev/null 2>&1 || { echo >&2 "docker-compose is required but it's not installed.  Aborting."; exit 1; }
-        docker-compose -f docker-compose-test.yml build && docker-compose -f docker-compose-test.yml up --abort-on-container-exit
+        type docker compose >/dev/null 2>&1 || { echo >&2 "docker compose is required but it's not installed.  Aborting."; exit 1; }
+        docker compose -f docker-compose-test.yml build && docker compose -f docker-compose-test.yml up --abort-on-container-exit
         ;;
     *)
         echo "Usage: subscription.sh {start|develop|test}" >&2
